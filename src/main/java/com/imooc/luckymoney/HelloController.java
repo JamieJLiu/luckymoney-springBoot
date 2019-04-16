8e7614f57768b45bf2fc8125278417e9f72d309d
JamieJLiu <151200040@smail.nju.edu.cn>
2019-04-16 11:03:54 +0800

    part2

--- src/main/java/com/imooc/luckymoney/HelloController.java
+++ src/main/java/com/imooc/luckymoney/HelloController.java
@@ -2,18 +2,24 @@ package com.imooc.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
     @Autowired
     private LimitConfig limitConfig;
 
-    @GetMapping("/hello")
-    public String say() {
-        return "说明：" + limitConfig.getDescription();
+    @GetMapping("/say/{id}")
+    public String say(@PathVariable("id") Integer id) {
+//      return "说明：" + limitConfig.getDescription();
+        return "id: " + id;
     }
+
 }
